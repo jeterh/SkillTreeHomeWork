b0387961bd526ed4fe4fe7465c9cf10fd7955891
Jeter <jeterh@gmail.com>
2016-04-10 18:26:20 +0800

    Init
    Init

---
+++ SkillTreeHomeWork/Controllers/Day1Controller.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkillTreeHomeWork.Models.ViewModels;
+
+namespace SkillTreeHomeWork.Controllers
+{
+    public class Day1Controller : Controller
+    {
+        // GET: Day1
+        public ActionResult Index()
+        {
+            List<Day1> viewModelDay1 = new List<Day1>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                string strCategory = "支出";
+
+                if (i % 2 == 0)
+                {
+                    strCategory = "收入";
+                }
+
+                var model = new Day1
+                {
+                    Day1ID = i,
+                    Category = strCategory,
+                    Money = 1000 * i,
+                    Date = DateTime.Now.AddDays(i)
+                };
+
+                viewModelDay1.Add(model);
+            }
+
+            return View(viewModelDay1);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Day1ChildAction()
+        {
+            List<Day1> viewModelDay1 = new List<Day1>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                string strCategory = "支出";
+
+                if (i % 3 == 0)
+                {
+                    strCategory = "收入";
+                }
+
+                var model = new Day1
+                {
+                    Day1ID = i,
+                    Category = strCategory,
+                    Money = 1000 * i,
+                    Date = DateTime.Now.AddDays(i)
+                };
+
+                viewModelDay1.Add(model);
+            }
+            
+            return View(viewModelDay1);
+        }
+
+
+    }
+}
---
+++ SkillTreeHomeWork/Models/ViewModels/Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkillTreeHomeWork.Models.ViewModels
+{
+    public class Day1
+    {
+        public int Day1ID { get; set; }
+        public string Category { get; set; }
+        public int Money { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
