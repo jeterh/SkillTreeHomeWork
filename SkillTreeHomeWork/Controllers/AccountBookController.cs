1586ac39b7fb4a707ce7f644b2ab547a26296322
Jeter <jeterh@gmail.com>
2016-04-19 23:59:19 +0800

    新增 AccountBook Model,View,Controller,DBContext

---
+++ SkillTreeHomeWork/Controllers/AccountBookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkillTreeHomeWork.Models.ViewModels;
+
+namespace SkillTreeHomeWork.Controllers
+{
+    public class AccountBookController : Controller
+    {
+        private AccountBookDbContext db = new AccountBookDbContext();
+
+        // GET: AccountBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult AccountBookChildAction()
+        {
+            return View(db.AccountBook.Take(10).ToList());
+        }
+
+    }
+}
---
+++ SkillTreeHomeWork/Models/ViewModels/AccountBookDbContext.cs
@@ -0,0 +1,21 @@
+namespace SkillTreeHomeWork.Models.ViewModels
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AccountBookDbContext : DbContext
+    {
+        public AccountBookDbContext()
+            : base("name=AccountBookDbContext")
+        {
+        }
+
+        public virtual DbSet<AccountBookModel> AccountBook { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
