aef73a6082cc137e1f77b6e0836cbdcc9fe826d4
Jeter <jeterh@gmail.com>
2016-04-19 23:54:51 +0800

    修正 回復Route指定預設為Home

--- SkillTreeHomeWork/App_Start/RouteConfig.cs
+++ SkillTreeHomeWork/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SkillTreeHomeWork
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "NoteBook", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
