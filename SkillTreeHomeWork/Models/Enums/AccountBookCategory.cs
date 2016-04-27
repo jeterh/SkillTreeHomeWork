87a0a0d5c4a8882bf4e5e446c2a3eff00b60c8aa
Jeter <jeterh@gmail.com>
2016-04-27 23:15:25 +0800

    新增 Category Enums

---
+++ SkillTreeHomeWork/Models/Enums/AccountBookCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SkillTreeHomeWork.Models.Enums
+{
+    public enum AccountBookCategory
+    {
+        [Display(Name = "支出")]
+        expend = 0,
+        [Display(Name = "收入")]
+        income = 1
+    }
+}
