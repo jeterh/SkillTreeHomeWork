fa117a569e57b72958c9ce98018c637327723039
Jeter <jeterh@gmail.com>
2016-04-27 23:38:52 +0800

    新增 Index Form Post 建立記事本資料,DropDownList 變更資料來源為Category Enums

--- SkillTreeHomeWork/Controllers/AccountBookController.cs
+++ SkillTreeHomeWork/Controllers/AccountBookController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SkillTreeHomeWork.Models.ViewModels;
+using System.Data.Entity;
+using System.Net;
 
 namespace SkillTreeHomeWork.Controllers
 {
@@ -20,12 +22,33 @@ namespace SkillTreeHomeWork.Controllers
         [ChildActionOnly]
         public ActionResult AccountBookChildAction()
         {
-            return View(db.AccountBook.Take(10).ToList());
+            return View(db.AccountBook.Take(10).OrderByDescending(x => x.Date).ToList());
+        }
+
+        // POST: AccountBook/Index
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Id,Category,Amount,Date,Remark")] AccountBookModels accountBookModels)
+        {
+            if (ModelState.IsValid)
+            {
+                accountBookModels.Id = Guid.NewGuid();
+                db.AccountBook.Add(accountBookModels);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(accountBookModels);
         }
 
         protected override void Dispose(bool disposing)
         {
-            db.Dispose();
+            if (disposing)
+            {
+                db.Dispose();
+            }
             base.Dispose(disposing);
         }
     }
