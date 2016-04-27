e59aa024cbb5449ca7eb452cb282c831e287f1d6
Jeter <jeterh@gmail.com>
2016-04-27 23:46:03 +0800

    修正 CustomValidationAttribute日期不得大於今天判斷

--- SkillTreeHomeWork/Models/CustomValidationAttribute/DateBeforeTodayAttribute.cs
+++ SkillTreeHomeWork/Models/CustomValidationAttribute/DateBeforeTodayAttribute.cs
@@ -10,14 +10,16 @@ namespace SkillTreeHomeWork.Models.CustomValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if (value != null)
+            if (value == null)
             {
-                DateTime myDateTime = DateTime.Parse(value.ToString());              
+                return new ValidationResult("請輸入日期。");
+            }
 
-                if (DateTime.Compare(myDateTime.Date, DateTime.Now.Date) == 1)
-                {
-                    return new ValidationResult("日期不得大於今天");
-                }
+            DateTime myDateTime = DateTime.Parse(value.ToString());              
+
+            if (DateTime.Compare(myDateTime.Date, DateTime.Now.Date) == 1)
+            {
+                return new ValidationResult("日期不得大於今天。");
             }
 
             return ValidationResult.Success;
