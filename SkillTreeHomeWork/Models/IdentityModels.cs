f30ad24a4336f0d75e8a41336d4a2f25d044a74d
Jeter <jeterh@gmail.com>
2016-05-16 17:22:35 +0800

    修正 Identity使用的Connection

--- SkillTreeHomeWork/Models/IdentityModels.cs
+++ SkillTreeHomeWork/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SkillTreeHomeWork.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AccountBookDbContext", throwIfV1Schema: false)
         {
         }
 
